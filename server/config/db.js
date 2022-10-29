4f6fd5431f147c9854aaceb889612b79feceee90
Naisarg Patel <npate658@my.centennialcollege.ca>
2022-10-29 19:18:43 -0400

    updated database URI to connect with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
 module.exports = {
   //local MongoDB deployment ->
-  URI:
-    "mongodb://127.0.0.1/book_store"
+  // URI:
+  //   "mongodb://127.0.0.1/book_store"
   // "URI": "mongodb://localhost/books229"
   // update the URI to the latest db
-  
+  URI: "mongodb+srv://naisarg:njpdbserver8731@midtermserver.b1g3vrr.mongodb.net/book_store?retryWrites=true&w=majority"
 };
 // db name : book_store
 // collection name : books
